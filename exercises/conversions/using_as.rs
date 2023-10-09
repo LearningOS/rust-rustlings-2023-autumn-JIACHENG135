edd30c7374128c015da5f4d0253d8c4a9eaccee2
JIACHENG135 <liulaoye135@tamu.edu>
2023-10-08 18:23:20 -0700

    [update] add more tests

--- exercises/conversions/using_as.rs
+++ exercises/conversions/using_as.rs
@@ -14,7 +14,7 @@
 
 fn average(values: &[f64]) -> f64 {
     let total = values.iter().sum::<f64>();
-    total / values.len()
+    total / values.len() as f64
 }
 
 fn main() {
@@ -29,5 +29,6 @@ mod tests {
     #[test]
     fn returns_proper_type_and_value() {
         assert_eq!(average(&[3.5, 0.3, 13.0, 11.7]), 7.125);
+        println!("passed");
     }
 }
