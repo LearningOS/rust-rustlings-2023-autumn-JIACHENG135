f0912bdaaeb04cb87ee2662e7f844bb52aec84e3
JIACHENG135 <liulaoye135@tamu.edu>
2023-10-08 18:10:14 -0700

    [ex] quiz 1,2,3

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -9,7 +9,7 @@
 // - An apple costs 2 rustbucks.
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
 // Write a function that calculates the price of an order of apples given the
-// quantity bought. No hints this time!
+// quantity bought.
 //
 // No hints this time ;)
 
@@ -17,7 +17,13 @@
 
 // Put your function here!
 // fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
